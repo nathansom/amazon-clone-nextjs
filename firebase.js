f3065bba6c1d558f7056729b5c01b0a679cffe7b
nathansom <nathan.sa.official@gmail.com>
2021-05-23 12:12:45 -0700

    fixed firebase info

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "",
+    apiKey: "AIzaSyDwkLcJPaxlnTC8zVtycOKHCikuHs12PdY",
     authDomain: "clone-nextjs-8adeb.firebaseapp.com",
     projectId: "clone-nextjs-8adeb",
     storageBucket: "clone-nextjs-8adeb.appspot.com",
-    messagingSenderId: "",
-    appId: ""
+    messagingSenderId: "88480159874",
+    appId: "1:88480159874:web:1f0059a10c57aa2036bc9b"
   };
