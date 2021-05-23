c9997e4e79ab694407f069f6c00747c44a4179c6
nathansom <nathan.sa.official@gmail.com>
2021-05-23 12:11:29 -0700

    delete credentials on firebase file

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDwkLcJPaxlnTC8zVtycOKHCikuHs12PdY",
+    apiKey: "",
     authDomain: "clone-nextjs-8adeb.firebaseapp.com",
     projectId: "clone-nextjs-8adeb",
     storageBucket: "clone-nextjs-8adeb.appspot.com",
-    messagingSenderId: "88480159874",
-    appId: "1:88480159874:web:1f0059a10c57aa2036bc9b"
+    messagingSenderId: "",
+    appId: ""
   };
